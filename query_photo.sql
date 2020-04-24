256d97e2d69dd10cb7656a322b8f3b83adf51152
Alex Clarke <informationcake@gmail.com>
2020-04-24 18:27:17 +0100

    Update query_photo.sql

--- query_photo.sql
+++ query_photo.sql
@@ -1,35 +1,35 @@
 select
 sa.objid, sa.ra, sa.dec, sa.type,
-sa.cmodelmag_r-sa.extinction_r AS cmod_r_corr,
-sa.psfmag_u-sa.extinction_u AS psf_u_corr,
-sa.psfmag_g-sa.extinction_g AS psf_g_corr,
-sa.psfmag_r-sa.extinction_r AS psf_r_corr,
-sa.psfmag_i-sa.extinction_i AS psf_i_corr,
-sa.psfmag_z-sa.extinction_z AS psf_z_corr,
+sa.cmodelmag_r AS cmod_r,
+sa.psfmag_u AS psf_u,
+sa.psfmag_g AS psf_g,
+sa.psfmag_r AS psf_r,
+sa.psfmag_i AS psf_i,
+sa.psfmag_z AS psf_z,
 w.w1mpro AS w1,
 w.w2mpro AS w2,
 w.w3mpro AS w3,
 w.w4mpro AS w4,
 xm.match_dist AS match_dist,
-pz.z, pz.zerr, pz.photoErrorClass from PhotoPrimary as sa
+pz.z, pz.zerr, pz.photoErrorClass into mydb.MyTable from PhotoPrimary as sa
 JOIN wise_xmatch AS xm ON sa.objid = xm.sdss_objid
 JOIN wise_allsky AS w ON xm.wise_cntr = w.cntr
 LEFT JOIN photoz as pz ON sa.objid = pz.objid
 WHERE
   sa.clean=1 and
   sa.specObjID=0 and
-  (sa.psfmag_u-sa.extinction_u < 35) and
-  (sa.psfmag_g-sa.extinction_g < 35) and
-  (sa.psfmag_r-sa.extinction_r < 35) and
-  (sa.psfmag_i-sa.extinction_i < 35) and
-  (sa.psfmag_z-sa.extinction_z < 35) and
-  (sa.psfmag_u-sa.extinction_u > 0) and
-  (sa.psfmag_g-sa.extinction_g > 0) and
-  (sa.psfmag_r-sa.extinction_r > 0) and
-  (sa.psfmag_i-sa.extinction_i > 0) and
-  (sa.psfmag_z-sa.extinction_z > 0) and
-  (sa.cmodelmag_r-sa.extinction_r <35) and
-  (sa.cmodelmag_r-sa.extinction_r >0) and
+  (sa.psfmag_u < 35) and
+  (sa.psfmag_g < 35) and
+  (sa.psfmag_r < 35) and
+  (sa.psfmag_i < 35) and
+  (sa.psfmag_z < 35) and
+  (sa.psfmag_u > 0) and
+  (sa.psfmag_g > 0) and
+  (sa.psfmag_r > 0) and
+  (sa.psfmag_i > 0) and
+  (sa.psfmag_z > 0) and
+  (sa.cmodelmag_r <35) and
+  (sa.cmodelmag_r >0) and
   (w.w1mpro<30) and
   (w.w2mpro<30) and
   (w.w3mpro<30) and
